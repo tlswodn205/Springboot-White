8808376c5ef436d3cfe9ebd99fb6061508774a3d
tlswodn205 <tlswodn205@naver.com>
2022-08-29 11:48:46 +0900

    래핑 공부 완료

---
+++ src/main/java/site/metacoding/white/FirstController.java
@@ -0,0 +1,39 @@
+package site.metacoding.white;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//Http method -> Mapping 기술
+//GET -> 주소창, 하이퍼링크
+//POST -> form 태그
+//PUT, DELETE -> JS
+
+//포스트맨 -> 4가지 요청을 쉽게 테스트 해볼 수 있다.
+@RestController //data 응답
+public class FirstController {
+	
+	
+	@GetMapping("/first")
+	public String getData() {
+		return "<h1>data</h1>";
+	}
+	
+	@PostMapping("/first")
+	public String postData() {
+		return "<h1>insert data</h1>";
+	}
+	
+	@PutMapping("/first")
+	public String putData() {
+		return "<h1>update data</h1>";
+	}
+	
+
+	@DeleteMapping("/first")
+	public String deleteData() {
+		return "<h1>delete data</h1>";
+	}
+}	
--- src/main/java/site/metacoding/white/WhiteApplication.java
+++ src/main/java/site/metacoding/white/WhiteApplication.java
@@ -1,4 +1,4 @@
-package site.metacoding.demo;
+package site.metacoding.white;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
